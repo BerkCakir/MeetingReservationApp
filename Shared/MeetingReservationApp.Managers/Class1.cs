bb2d521f53d3d8b0aa510a71e22502aeee0a7038
Berk Cakir (Dogus Teknoloji) <BerkC@d-teknoloji.com.tr>
2021-10-04 22:18:27 +0300

    Data, Entities and Managers projects created

---
+++ Shared/MeetingReservationApp.Managers/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MeetingReservationApp.Managers
+{
+    public class Class1
+    {
+    }
+}
--- Shared/MeetingReservationApp.Shared/Abstract/Entities/EntityBase.cs
+++ Shared/MeetingReservationApp.Shared/Abstract/Entities/EntityBase.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReservationApp.Shared.Entites.Abstract
+namespace MeetingReservationApp.Shared.Entites.Abstract
 {
     public abstract class EntityBase
     {
