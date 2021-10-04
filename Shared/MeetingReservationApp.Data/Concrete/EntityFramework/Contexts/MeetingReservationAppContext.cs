82e2925ef0df57bdcf4cad4455f5e2e968c26157
Berk Cakir (Dogus Teknoloji) <BerkC@d-teknoloji.com.tr>
2021-10-05 00:10:40 +0300

    custom db context was added with mappings

---
+++ Shared/MeetingReservationApp.Data/Concrete/EntityFramework/Contexts/MeetingReservationAppContext.cs
@@ -0,0 +1,33 @@
+﻿using MeetingReservationApp.Data.Concrete.EntityFramework.Mappings;
+using MeetingReservationApp.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingReservationApp.Data.Concrete.EntityFramework.Contexts
+{
+    public class MeetingReservationAppContext : DbContext
+    {
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<RoomReservation> RoomReservations { get; set; }
+        public DbSet<InventoryReservation> InventoryReservations { get; set; }
+
+        public MeetingReservationAppContext(DbContextOptions<MeetingReservationAppContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new LocationMap());
+            modelBuilder.ApplyConfiguration(new RoomMap());
+            modelBuilder.ApplyConfiguration(new InventoryMap());
+            modelBuilder.ApplyConfiguration(new RoomReservationMap());
+            modelBuilder.ApplyConfiguration(new InventoryReservationMap());
+        }
+    }
+}
