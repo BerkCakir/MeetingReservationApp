2412d16c59ab9824e03de3d3c87f4d09d817320e
Berk Cakir (Dogus Teknoloji) <BerkC@d-teknoloji.com.tr>
2021-10-05 23:08:27 +0300

    Specific implementations of IEntityRepository - if a specific method is needed for a specific repository - extend this classes

---
+++ Shared/MeetingReservationApp.Data/Abstract/IInventoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using MeetingReservationApp.Entities.Concrete;
+using MeetingReservationApp.Shared.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingReservationApp.Data.Abstract
+{
+    public interface IInventoryRepository : IEntityRepository<Inventory>
+    {
+    }
+}
