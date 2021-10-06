02a6ca6495c9439306b8edfb3cbd4cfc46de2c83
Berk Cakir (Dogus Teknoloji) <BerkC@d-teknoloji.com.tr>
2021-10-06 22:27:29 +0300

    Empty manager classes and interfaces were added

---
+++ Shared/MeetingReservationApp.Managers/Concrete/InventoryReservationManager.cs
@@ -0,0 +1,14 @@
+﻿using MeetingReservationApp.Managers.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingReservationApp.Managers.Concrete
+{
+    public class InventoryReservationManager : IInventoryReservationService
+    {
+      
+    }
+}
---
+++ Shared/MeetingReservationApp.Managers/Concrete/RoomReservationManager.cs
@@ -0,0 +1,13 @@
+﻿using MeetingReservationApp.Managers.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingReservationApp.Managers.Concrete
+{
+    public class RoomReservationManager : IRoomReservationService
+    {
+    }
+}
