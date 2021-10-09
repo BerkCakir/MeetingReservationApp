9a8cbaf824de8b4acad355ce93113f2075c61b0b
Berk Cakir (Dogus Teknoloji) <BerkC@d-teknoloji.com.tr>
2021-10-09 22:48:15 +0300

    controller result structure overloaded for result with data

--- Shared/MeetingReservationApp.Shared/ControllerBases/CustomControllerBase.cs
+++ Shared/MeetingReservationApp.Shared/ControllerBases/CustomControllerBase.cs
@@ -7,6 +7,26 @@ namespace MeetingReservationApp.Shared.ControllerBases
 {
     public class CustomControllerBase : ControllerBase
     {
+        public IActionResult CreateResultWithData<T>(IDataResult<T> response)
+        {
+            int statusCode = 0;
+            switch (response.ResultStatus)
+            {
+                case ResultStatus.Success:
+                    statusCode = (int)HttpStatusCode.OK;
+                    break;
+                case ResultStatus.Error:
+                    statusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+                default:
+                    statusCode = (int)HttpStatusCode.NotFound;
+                    break;
+            }
+            return new ObjectResult(response)
+            {
+                StatusCode = statusCode
+            };
+        }
         public IActionResult CreateResult(IResult response)
         {
             int statusCode;
