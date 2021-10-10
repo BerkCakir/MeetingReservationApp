4c5576c38cba461ace05a989f0892321da32c466
Berk Cakir (Dogus Teknoloji) <BerkC@d-teknoloji.com.tr>
2021-10-10 18:33:25 +0300

    options pattern applied for reading settings from appsettings.json

---
+++ Frontends/MeetingReservationApp.Web/Models/ServiceApiSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingReservationApp.Web.Models
+{
+    public class ServiceApiSettings
+    {
+        public string IdentityBaseUri { get; set; }
+        public string GatewayBaseUri { get; set; }
+        public ServiceApi RoomReservation { get; set; }
+        public ServiceApi InventoryReservation { get; set; }
+    }
+
+    public class ServiceApi
+    {
+        public string Path { get; set; }
+    }
+}
